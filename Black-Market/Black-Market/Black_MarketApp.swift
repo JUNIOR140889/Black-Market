4bfe20a2aee746c56eec5f3ef44a58a088c30694
Junior Sancho <junior.sancho@rootstrap.com>
2023-07-10 22:06:19 -0300

    Project created

---
+++ Black-Market/Black-Market/Black_MarketApp.swift
@@ -0,0 +1,17 @@
+//
+//  Black_MarketApp.swift
+//  Black-Market
+//
+//  Created by Junior Sancho on 7/10/23.
+//
+
+import SwiftUI
+
+@main
+struct Black_MarketApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
